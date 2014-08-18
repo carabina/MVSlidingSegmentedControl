7f9e5a29d2a77c9546f6b7d6001bb8bb6a1eca87
Andrea Bizzotto <andrea@snupps.com>
2014-08-18 10:32:57 +0100

    Initial Commit

---
+++ MVSlidingSegmentedControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MVSlidingSegmentedControl
+//
+//  Created by Andrea Bizzotto on 18/08/2014.
+//  Copyright (c) 2014 musevisions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MVSlidingSegmentedControlTests/MVSlidingSegmentedControlTests.m
@@ -0,0 +1,34 @@
+//
+//  MVSlidingSegmentedControlTests.m
+//  MVSlidingSegmentedControlTests
+//
+//  Created by Andrea Bizzotto on 18/08/2014.
+//  Copyright (c) 2014 musevisions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MVSlidingSegmentedControlTests : XCTestCase
+
+@end
+
+@implementation MVSlidingSegmentedControlTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
